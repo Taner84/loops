763372aa40394fb15a36f5c41992f263cb2b4de9
Taner <taner@taner-k.nl>
2024-03-05 13:05:14 +0000

    add previous code

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const double = (numbers) => {
-  let result = []
-    for (const number of numbers){
-      result.push(number * 2)
+const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (const number1 of numbers1){
+  console.log(number1)
+}
+
+
+const double = (numbers2) => {
+  let result2 = []
+    for (const number2 of numbers2){
+      result2.push(number2 * 2)
     }
-  return result
+  return result2
 }
 
 console.log(double([1, 2, 3, 4, 5, 6]))
