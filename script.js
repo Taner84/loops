66f7d1f3176cf94425318d88dcefc89ec974b82f
Taner <taner@taner-k.nl>
2024-03-05 13:01:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const double = (numbers) => {
+  let result = []
+    for (const number of numbers){
+      result.push(number * 2)
+    }
+  return result
+}
+
+console.log(double([1, 2, 3, 4, 5, 6]))
